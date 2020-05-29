47e1f6599f9b75ecd198d4e9afa35f990404982c
Eric Tang <jivesushi@yahoo.com>
2020-05-28 22:45:24 -0400

    add SortMainDisplay component

--- client/App.js
+++ client/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import BSTMainDisplay from './components/BSTMainDisplay';
+import SortMainDisplay from './components/SortMainDisplay';
 const { UserContextProvider } = require("./context/UserContext");
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <div>
         <Header />
         <BSTMainDisplay />
+        <SortMainDispaly />
       </div>
     </UserContextProvider>
   );
---
+++ client/components/SortMainDisplay.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as d3 from "d3";
+import '../stylesheets/style.css';
+
+const SortMainDisplay = () => {
+
+  return (
+    <div>
+      <h3>SortMainDisplay</h3>
+    </div>
+  );
+}
+ 
+export default SortMainDisplay;
