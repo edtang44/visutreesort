c2c90c91914905b2850657b51cf490910f9608b6
Eric Tang <jivesushi@yahoo.com>
2020-06-04 13:31:23 -0400

    refactor functionality

--- client/components/SortAlgoDisplay.js
+++ client/components/SortAlgoDisplay.js
@@ -19,56 +19,54 @@ const SortAlgoDisplay = () => {
     width = 960 - margin.left - margin.right,
     height = 5000 - margin.top - margin.bottom;
   let barWidth = width/count;
-  let x = d3.scaleLinear()
-    .domain([0,count])
+  let xScale = d3.scaleLinear()
+    .domain([0, count])
     .range([0, width]);
   let svg, rects, labels;
 
   useEffect(() => {
     removeChart();
     durationTime = time/20;
-    svg = d3.select("#sortchart").append("svg")
-      .attr('id', 'currentChart')
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-
-    rects = svg.append("g")
-      .attr("transform", "translate(" + barWidth + ",2)")
-      .selectAll("rect")
-      .data(unsortedArray)
-      .enter()
-      .append("rect")
-    
-    labels = svg.selectAll("text")
-      .data(unsortedArray)
-      .enter().append("text")
-
-      labels.attr("id", function(d) {return "text" + d})
-        .attr("transform", function(d, i) {return "translate(" + x(i) + ",0)"})
-        .html(function(d) {return d;})
-    
-      rects.attr("id", function(d) {return "rect" + d})
-        .attr("transform", function(d, i) {return "translate(" + (x(i) - barWidth) + ",0)"})
-        .attr("width", barWidth *.9)
-        .attr("height", function(d) {return d*barWidth/3})
+    displayChart();
     }, [count, sortDisplay, time]);
+
+  function displayChart() {
+    svg = d3.select("#sortchart").append("svg")
+    .attr('id', 'currentChart')
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+  rects = svg.append("g")
+    .attr("transform", "translate(" + barWidth + ",2)")
+    .selectAll("rect")
+    .data(unsortedArray)
+    .enter()
+    .append("rect")
+  labels = svg.selectAll("text")
+    .data(unsortedArray)
+    .enter().append("text")
+  rects.attr("id", function(d) {return "rect" + d})
+    .attr("transform", function(d, i) {return "translate(" + (xScale(i) - barWidth) + ",0)"})
+    .attr("width", barWidth *.9)
+    .attr("height", function(d) {return d*barWidth/3})
+
+  labels.attr("id", function(d) {return "text" + d})
+    .attr("transform", function(d, i) {return "translate(" + xScale(i) + ",0)"})
+    .html(function(d) {return d;})
+  }
 
   function reset() {
     unsortedArray = [...array];
     sortedArray = [];
     stop = false;
-
     d3.select("#counter").html(steps = 0)
-
     labels.attr("class", "")                
         .transition().duration(2000)
-        .attr("transform", function(d, i) {return "translate(" + (x(i)) + ", 0)"})              
-    
+        .attr("transform", function(d, i) {return "translate(" + (xScale(i)) + ", 0)"})              
     rects.attr("class", "")                
         .transition().duration(2000)
-        .attr("transform", function(d, i) {return "translate(" + (x(i-1)) + ", 0)"})
+        .attr("transform", function(d, i) {return "translate(" + (xScale(i-1)) + ", 0)"})
   }
 
   function mergeSort() {
@@ -111,23 +109,18 @@ const SortAlgoDisplay = () => {
         const newArray = [...mergeArrays[j+1]];
         const sortedArray = [];
         moveStep(0);
-
         function moveStep(n) {
             if (stop) return stop = false;            
             d3.selectAll("rect").attr("class", "")                
-
             d3.select("#counter").html(++steps);
             d3.select("#rect" + newArray[n]).attr("class", "testing")
-
             sortedArray.push(newArray[n])
             oldArray.shift()
-
             rects.transition().duration(durationTime)
                 .attr("transform", function(d) {
                     const xVal = sortedArray.indexOf(d) > -1 ? sortedArray.indexOf(d) : oldArray.indexOf(d) + sortedArray.length;
-                    return "translate(" + x(xVal - 1) + ",0)" 
+                    return "translate(" + xScale(xVal - 1) + ",0)" 
                 })
-
             labels
                 .classed("sorted", function(d) {
                     return !mergeArrays[j + 2] && sortedArray.indexOf(d) == d - 1;
@@ -135,9 +128,8 @@ const SortAlgoDisplay = () => {
                 .transition().duration(durationTime)
                 .attr("transform", function(d) {
                     const xVal = sortedArray.indexOf(d) > -1 ? sortedArray.indexOf(d) : oldArray.indexOf(d) + sortedArray.length;
-                    return "translate(" + x(xVal) + ",0)" 
+                    return "translate(" + xScale(xVal) + ",0)" 
                 })
-
             d3.timeout(function() {
                 if (oldArray.length > 0) {
                     moveStep(++n)
@@ -155,15 +147,12 @@ const SortAlgoDisplay = () => {
     const value = unsortedArray.shift();
     sortedArray.push(value);      
     reArrange(sortedArray.length-1);
-
     function reArrange(n) {
         if (stop) return stop = false;            
-
         d3.selectAll("rect").attr("class", "")                
         d3.select("#rect" + value).attr("class", "testing")
         d3.select("#text" + value).attr("class", "sorted")     
         d3.select("#counter").html(++steps);
-        
         if (n > 0 && sortedArray[n-1] > value) {
             d3.timeout(function() {
                 sortedArray.splice(n,1);
@@ -178,62 +167,45 @@ const SortAlgoDisplay = () => {
             return d3.selectAll("rect").attr("class", "")
           }
     }
-
   }
 
   function selectionSort() {
     let min = count;
     let spliceIndex;
     let i = 0;
-
     function findMin() {
         if (stop) return stop = false;
-
         d3.timeout(function() {
-        
             if (i<=unsortedArray.length) {
-
                 d3.select("#rect" + unsortedArray[i]).attr("class", "testing")
-
                 d3.timeout(function() {
-                    
                     d3.select("#rect" + unsortedArray[i]).attr("class", "")
-
                     if (unsortedArray[i] < min) {
                         d3.select("#rect" + unsortedArray[i]).attr("class", "min")
                         d3.select("#rect" + min).attr("class", "")
                         min = unsortedArray[spliceIndex = i]
                     }
-
                     d3.select("#counter").html(++steps);
                     i++;
-
                     d3.timeout(function() {return findMin()}, durationTime/2);
-
                 }, durationTime/2);
-
             } else {
                 sortedArray.push(min);
                 unsortedArray.splice(spliceIndex,1);
-
                 d3.select("#counter").html(++steps);
-
-            rects.transition().duration(durationTime * 4)
-                .attr("transform", function(d) {
+                rects.transition().duration(durationTime * 4)
+                  .attr("transform", function(d) {
                     const xVal = sortedArray.indexOf(d) > -1 ? sortedArray.indexOf(d) : unsortedArray.indexOf(d) + sortedArray.length;
-                    return "translate(" + x(xVal - 1) + ",0)" 
+                    return "translate(" + xScale(xVal - 1) + ",0)" 
                 })
-
-            labels
-                .classed("sorted", function(d) {return sortedArray.indexOf(d) == d - 1;})
-                .transition().duration(durationTime * 4)
-                .attr("transform", function(d) {
-                    const xVal = sortedArray.indexOf(d) > -1 ? sortedArray.indexOf(d) : unsortedArray.indexOf(d) + sortedArray.length;
-                    return "translate(" + x(xVal) + ",0)" 
-                })
-
+                labels
+                  .classed("sorted", function(d) {return sortedArray.indexOf(d) == d - 1;})
+                  .transition().duration(durationTime * 4)
+                  .attr("transform", function(d) {
+                      const xVal = sortedArray.indexOf(d) > -1 ? sortedArray.indexOf(d) : unsortedArray.indexOf(d) + sortedArray.length;
+                      return "translate(" + xScale(xVal) + ",0)" 
+                  })
                 rects.attr("class", "")
-
                 d3.timeout(function() {
                     if (unsortedArray.length > 0) selectionSort();
                 }, durationTime);
@@ -245,44 +217,32 @@ const SortAlgoDisplay = () => {
   }
 
   function bubbleSort() {
-
     function sortPass(i) {
         if (!unsortedArray.length || stop) return stop = false
-
         if (i<=unsortedArray.length) {
             if (unsortedArray[i] < unsortedArray[i-1]) {
-
                 d3.select("#rect" + unsortedArray[i]).attr("class", "testing")
                 d3.select("#rect" + unsortedArray[i-1]).attr("class", "testing")
-                
                 d3.timeout(function() {
                     d3.select("#rect" + unsortedArray[i]).attr("class", "")
                     d3.select("#rect" + unsortedArray[i-1]).attr("class", "")                                            
                 }, durationTime);
-
                 const temp = unsortedArray[i-1];
                 unsortedArray[i-1] = unsortedArray[i];
                 unsortedArray[i] = temp;
-
                 slide(unsortedArray[i], i + sortedArray);
                 slide(unsortedArray[i-1], i-1 + sortedArray);
-
                 d3.select("#counter").html(++steps);
-
                 d3.timeout(function() {return sortPass(++i)}, durationTime);
-
             } else if (i == unsortedArray.length) {
-
                 for (let n = i; n == unsortedArray[n-1]; n--) {
                     d3.select("#text" + n).attr("class", "sorted")
                     unsortedArray.pop();
                 }              
-
                 sortPass(++i);
             } else {               
                 sortPass(++i);
             }
-
         } else {
             bubbleSort();
         }
@@ -293,11 +253,10 @@ const SortAlgoDisplay = () => {
   function slide(d, i) {
     d3.select("#text" + d)
         .transition().duration(durationTime)
-        .attr("transform", function(d) {return "translate(" + (x(i)) + ", 0)"})
-
+        .attr("transform", function(d) {return "translate(" + (xScale(i)) + ", 0)"})
     d3.select("#rect" + d)
         .transition().duration(durationTime)
-        .attr("transform", function(d) {return "translate(" + (x(i-1)) + ", 0)"})                
+        .attr("transform", function(d) {return "translate(" + (xScale(i-1)) + ", 0)"})                
   }
 
   function removeChart() {
@@ -322,7 +281,7 @@ const SortAlgoDisplay = () => {
   return (
     <>
     <div>
-      <label htmlFor="numNodes">Change Number of Elements in Array (3-50)  </label>
+      <label htmlFor="numNodes">Number of Elements (3-50)  </label>
       <input type="number" name="numElements" value={Number(count)- 1} min="3" max="50" onChange={handleChange} />
       <Button variant="contained" color="primary" id='new-chart' 
         onClick={() => {
