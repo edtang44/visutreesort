e809aeb10c6a19ad0191554545edfdbedc1a9e2f
Eric Tang <jivesushi@yahoo.com>
2020-07-12 15:32:58 -0700

    move HashRouter to App

--- client/App.js
+++ client/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import SortAlgoDisplay from './components/SortAlgoDisplay';
 import TreeDisplay from './components/TreeDisplay';
 
 const App = () => {
   return (
-      <div className="AppLayout">
-        <Switch>
-          <Route exact path="/" component={ Header } />
-          <Route exact path="/sort" component={ SortAlgoDisplay } />
-          <Route exact path="/bst" component={ TreeDisplay } />
-        </Switch>
-      </div>
+      <HashRouter>
+        <div className="AppLayout">
+          <Switch>
+            <Route exact path="/" component={ Header } />
+            <Route exact path="/sort" component={ SortAlgoDisplay } />
+            <Route exact path="/bst" component={ TreeDisplay } />
+          </Switch>
+        </div>
+      </HashRouter>
   );
 }
  
--- client/index.js
+++ client/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <HashRouter basename='/'>
-    <App />
-  </HashRouter>,
+    <App />,
   rootElement
 );
