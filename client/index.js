cadf84d55cb841a9ee925ec957c5d5fa1496029a
Eric Tang <jivesushi@yahoo.com>
2020-07-12 15:24:51 -0700

    modify HashRouter rename

--- client/index.js
+++ client/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <Router>
+  <HashRouter>
     <App />
-  </Router>,
+  </HashRouter>,
   rootElement
 );
