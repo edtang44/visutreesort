6f23fa5372292bb15b2238ad80ffaa1cdd9b19bd
Eric Tang <jivesushi@yahoo.com>
2021-01-08 22:56:04 -0800

    code split routes using Suspense and use Spinner as fallback

--- client/App.js
+++ client/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
-import Header from './components/Header';
-import SortAlgoDisplay from './components/SortAlgoDisplay';
-import TreeDisplay from './components/TreeDisplay';
+import SpinnerDisplay from './components/SpinnerDisplay';
+const Header = React.lazy(() => import('./components/Header'));
+const SortAlgoDisplay = React.lazy(() => import('./components/SortAlgoDisplay'));
+const TreeDisplay = React.lazy(() => import('./components/TreeDisplay'));
 
 const App = () => {
   return (
+    <React.Suspense fallback={<SpinnerDisplay />}>
       <HashRouter>
         <div className="AppLayout">
           <Switch>
@@ -15,7 +17,6 @@ const App = () => {
           </Switch>
         </div>
       </HashRouter>
+    </React.Suspense>
   );
 }
- 
-export default App;
