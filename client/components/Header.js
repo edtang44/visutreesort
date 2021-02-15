877772c416953420e86e389cbaf504285dd35a32
Eric Tang <jivesushi@yahoo.com>
2021-02-15 13:43:47 -0500

    add responsive header

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import {useSpring, animated} from 'react-spring'
 import '../stylesheets/style.css';
-import { Flex, Heading, Button, ButtonGroup } from '@chakra-ui/react';
+import { VStack, Flex, Heading, Button } from '@chakra-ui/react';
 import { Text } from '@chakra-ui/core';
 
 const Header = () => {
   const props = useSpring({opacity: 1, from: {opacity: 0}})
   return (
     <>
-      <Flex w="100%" h="100px"/>
+      <VStack marginTop="100px" w="100%" h="100px" spacing="75px">
       <animated.div style={props}>
         <Heading as="h1" size="2xl"> 
           VisuTreeSort 
@@ -17,14 +17,15 @@ const Header = () => {
         <Text as="i" fontSize="30px">visualizing binary tree traversals and sorting algorithms through animation</Text>
       </animated.div>
       <Flex w="100%" h="100px"/>
-      <ButtonGroup colorScheme="blue" size="lg" variant="outline" spacing="6">
-        <Button as={Link} to="/bst">
+      <Flex direction={['column', 'column', 'row', 'row']} justify="center">
+        <Button colorScheme="blue" size="lg" variant="outline" spacing="6" as={Link} to="/bst">
           Binary Tree Traversal
         </Button>   
-        <Button as={Link} to="/sort">
+        <Button colorScheme="blue" size="lg" variant="outline" spacing="6" as={Link} to="/sort">
           Sorting Algorithms
         </Button>   
-      </ButtonGroup>
+      </Flex>
+      </VStack>
     </>
   );
 }
